6f20ef1f429469cf2ee384f74c6ace526cb383db
Given Loyiso <info@givenloyiso.com>
2021-09-11 12:56:23 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from './features/car/carSlice';
+import { useSelector } from 'react-redux';
+
+function Header() {
+	const [burgerStatus, setBurgerStatus] = useState(false);
+	const cars = useSelector(selectCars);
+
+	return (
+		<Container>
+			<a>
+				<img src='/images/logo.svg' alt='' />
+			</a>
+			<Menu>
+				{cars && cars.map((car, index) =>
+					<a href='#'>{car}</a>
+				)}
+			</Menu>
+
+			<RightMenu>
+				<a href='#'>Shop</a>
+				<a href='#'>Tesla account</a>
+				<CustomMenu onClick={() => setBurgerStatus(true)} />
+			</RightMenu>
+			<BurgerNav show={burgerStatus}>
+				<CloseWrapper>
+					<CustomClose onClick={() => setBurgerStatus(false)} />
+				</CloseWrapper>
+				<li>
+					<a href='#'>Existing Inventory</a>
+				</li>
+				<li>
+					<a href='#'>Used Inventory</a>
+				</li>
+				<li>
+					<a href='#'>Trade-in</a>
+				</li>
+				<li>
+					<a href='#'>Cybertruck</a>
+				</li>
+				<li>
+					<a href='#'>Roadaster</a>
+				</li>
+			</BurgerNav>
+		</Container>
+	);
+}
+
+export default Header;
+
+const Container = styled.div`
+	min-height: 60px;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 0 20px;
+	z-index: 1;
+
+	img {
+		cursor: pointer;
+		width: 100px;
+		height: 14px;
+	}
+`;
+
+const Menu = styled.div`
+	flex: 1;
+	a {
+		font-weight: 600;
+		text-transform: uppercase;
+		padding: 0 10px;
+		flex: nowrap;
+	}
+
+	@media (max-width: 768px) {
+		display: none;
+	}
+`;
+
+const RightMenu = styled.div`
+	display: flex;
+	align-items: center;
+	a {
+		font-weight: 600;
+		text-transform: uppercase;
+		margin-right: 10px;
+	}
+`;
+
+const CustomMenu = styled(MenuIcon)``;
+
+const BurgerNav = styled.div`
+	position: fixed;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	background-color: #fff;
+	width: 300px;
+	z-index: 50;
+	list-style: none;
+	padding: 20px;
+	flex-direction: column;
+	text-align: left;
+	display: flex;
+	transform: ${(props) => (props.show ? 'translateX(0)' : 'translateX(300px)')};
+	transition: 0.3s ease-in-out;
+
+	li {
+		padding: 15px 0;
+		border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+		a {
+			font-weight: 600;
+		}
+	}
+`;
+
+const CustomClose = styled(CloseIcon)`
+	cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+	display: flex;
+	justify-content: flex-end;
+`;
